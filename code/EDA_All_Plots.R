fc8c2eddccf4fa8ed29f2d16a7da84c29c079316
Weipo Lai <80244742+weipo0116@users.noreply.github.com>
2024-06-12 16:25:35 +0800

    Add files via upload

--- code/EDA_All_Plots.R
+++ code/EDA_All_Plots.R
@@ -240,3 +240,39 @@ is_fraudulent_plots <- function(data, path = "./image/") {
 }
 
 is_fraudulent_plots(data)
+
+data$Is.Fraudulent <- as.factor(data$Is.Fraudulent)
+data$Transaction_Amount <-
+  cut(
+    data$Transaction.Amount,
+    breaks = c(0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, Inf),
+    labels = c(
+      "0-0.5k",
+      "0.5k-1k",
+      "1k-1.5k",
+      "1.5k-2k",
+      "2k-2.5k",
+      "2.5k-3k",
+      "3k-3.5k",
+      "3.5k-4k",
+      "4k+"
+    )
+  )
+data$Account_Age_Days_Period <- cut(data$Account.Age.Days, breaks = pretty(range(data$Account.Age.Days), n = 10))
+columns <-c('Transaction.Hour', 'Account_Age_Days_Period')
+
+for (col in columns) {
+  heatmap <-
+    ggplot(data,
+           aes(x = Transaction_Amount, y = factor(!!sym(col)), fill = Is.Fraudulent)) +
+    geom_tile(color = "grey", alpha = 0.5) +
+    scale_fill_manual(values = c("blue", "orange"))  # 自定義填充顏色
+  plot_title <-
+    paste("Transaction Amount vs.", col)
+  ggsave(
+    filename = paste0("./image/", "heatmap_", gsub(" ", "_", col), ".png"),
+    plot = heatmap,
+    width = 8,
+    height = 6
+  )
+}
